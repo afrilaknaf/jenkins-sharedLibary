900acf36b96da35931905f57cf0ba402ec31ea59
afrilaknaf <afrilaknaf036@gmail.com>
2026-07-21 16:33:36 +0530

    small -2 change

--- vars/installpackage.groovy
+++ vars/installpackage.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]){
     bat "echo package Manager:${config.packageManager}"
-    bat "echo ${config.packageManager} install " 
+    bat "${config.packageManager} install " 
 }
